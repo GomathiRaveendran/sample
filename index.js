8ec0c6b5f3a330076902178195c2d81d0283d8a4
GomathiRaveendran <59409466+GomathiRaveendran@users.noreply.github.com>
2020-05-13 15:36:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+function onein(el) {
+  el.innerHTML = "About Me";
+}
+function oneout(el) {
+  el.innerHTML = "Gomathi Raveendran";
+}
+
+function twoin(el) {
+  el.innerHTML = "Blogs, Github and more..";
+}
+function twoout(el) {
+  el.innerHTML = "Aspirations";
+}
+function threein(el) {
+  var d = new Date();
+  el.innerHTML =
+    "As of " +
+    d.getDate() +
+    " / " +
+    month[d.getMonth()] +
+    " / " +
+    d.getFullYear();
+}
+function threeout(el) {
+  el.innerHTML = "Accomplishments";
+}
+
+function frin(el)
+{
+  document.getElementsByClassName('peek')[0].src=el.href;
+  document.getElementsByClassName('peek')[1].src=el.href;
+}
+function frout() {document.getElementsByClassName('peek')[0].src=''; document.getElementsByClassName('peek')[1].src='';}
+
+var month = [
+  "Dummy",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec"
+];
